c65d1e93f30ff2f1cfaa3a7dc7f39d4054f52be2
MadInc63 <MadInc63@gmail.com>
2018-01-04 23:37:15 +0300

    rebuild script

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var TIMEOUT_IN_SECS = 3 * 60
+var ALERT_TIMEOUT_IN_SECS = 30
 var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var ALERT_MESSAGE = ["Неудача – это просто возможность начать снова, но уже более мудро. © Генри Форд",
+  "Если проблему можно разрешить, не стоит о ней беспокоиться. Если проблема неразрешима, беспокоиться о ней бессмысленно. © Далай Лама",
+  "Даже если вы очень талантливы и прилагаете большие усилия, для некоторых результатов просто требуется время: вы не получите ребенка через месяц, даже если заставите забеременеть девять женщин. © Уоррен Баффет",
+  "Раз в жизни фортуна стучится в дверь каждого человека, но человек в это время нередко сидит в ближайшей пивной и никакого стука не слышит. © Марк Твен",
+  "Наш большой недостаток в том, что мы слишком быстро опускаем руки. Наиболее верный путь к успеху – все время пробовать еще один раз. © Томас Эдисон",
+  "Лично я люблю землянику со сливками, но рыба почему-то предпочитает червяков. Вот почему, когда я иду на рыбалку, я думаю не о том, что люблю я, а о том, что любит рыба. © Дейл Карнеги",
+  "Просыпаясь утром, спроси себя: «Что я должен сделать?» Вечером, прежде чем заснуть: «Что я сделал?». © Пифагор",
+  "Бедный, неудачный, несчастливый и нездоровый это тот, кто часто использует слово «завтра». © Роберт Кийосаки",
+  "Старики всегда советуют молодым экономить деньги. Это плохой совет. Не копите пятаки. Вкладывайте в себя. Я в жизни не сэкономил и доллара, пока не достиг сорока лет. © Генри Форд",
+  "Я этого хочу. Значит, это будет. © Генри Форд",
+  "Я не терпел поражений. Я просто нашёл 10 000 способов, которые не работают. © Томас Эдисон",
+  "Раньше я говорил: «Я надеюсь, что все изменится». Затем я понял, что существует единственный способ, чтобы все изменилось— измениться мне самому. © Джим Рон",
+  "Урок, который я извлек и которому следую всю жизнь, состоял в том, что надо пытаться, и пытаться, и опять пытаться – но никогда не сдаваться! © Ричард Бренсон"];
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
@@ -11,6 +25,11 @@ class Timer{
   constructor(timeout_in_secs){
     this.initial_timeout_in_secs = timeout_in_secs
     this.reset()
+  }
+  setSecsLeft(timeout_in_secs) {
+    this.initial_timeout_in_secs = timeout_in_secs
+    this.timeout_in_secs = this.initial_timeout_in_secs
+    this.timestampOnStart = this.getTimestampInSecs()
   }
   getTimestampInSecs(){
     var timestampInMilliseconds = new Date().getTime()
@@ -56,7 +75,7 @@ class TimerWidget{
     // adds HTML tag to current page
     this.timerContainer = document.createElement('div')
 
-    this.timerContainer.setAttribute("style", "height: 100px;")
+    this.timerContainer.setAttribute("style", "height: 70px;top: 10px;left: 10px;position: fixed;z-index: 9999;margin-left: 0px;padding-left: 10px;padding-right: 10px;background-color: #00000060;color: white;text-shadow: 0 0 20px black;")
     this.timerContainer.innerHTML = TEMPLATE
 
     rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
@@ -79,6 +98,14 @@ class TimerWidget{
   }
 }
 
+function randomNumberFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function alertMessage() {
+  var randomMessage = ALERT_MESSAGE[randomNumberFromInterval(0, ALERT_MESSAGE.length)]
+  alert(randomMessage)
+}
 
 function main(){
 
@@ -91,6 +118,10 @@ function main(){
   function handleIntervalTick(){
     var secsLeft = timer.calculateSecsLeft()
     timerWiget.update(secsLeft)
+    if (secsLeft <= 0) {
+        alertMessage()
+        timer.setSecsLeft(ALERT_TIMEOUT_IN_SECS)
+    }
   }
 
   function handleVisibilityChange(){
